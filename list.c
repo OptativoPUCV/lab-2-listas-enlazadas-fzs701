8e9874e3b9437909bf99cfe2b9b8b2f6656f3ecb
Your Name <you@example.com>
2025-03-26 08:48:55 -0300

    new try

--- list.c
+++ list.c
@@ -57,7 +57,7 @@ void * nextList(List * list) {
 
 void * lastList(List * list) {
     if(list == NULL || list->tail == NULL){
-        retun NULL;
+        return NULL;
     }
     list->current = list->tail;
     return list->current->data;
